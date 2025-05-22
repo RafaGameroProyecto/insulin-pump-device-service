38bcc3801d99418886ad255d7717d30a2c15203f
Rafa-Gamero <rafagameroarrabal@gmail.com>
2025-05-22 12:59:19 +0200

    commit inicial

---
+++ src/main/java/com/insulinpump/deviceservice/DeviceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.insulinpump.deviceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceserviceApplication.class, args);
+	}
+
+}
